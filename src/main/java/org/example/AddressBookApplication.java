aa0faa8054d08a68ca23b4a2a2d5fd0cfbb82166
TheRNB <aranbateni@gmail.com>
2025-09-19 11:42:01 -0400

    minor fixes

--- src/main/java/org/example/AddressBookApplication.java
+++ src/main/java/org/example/AddressBookApplication.java
@@ -1,11 +1,37 @@
 package org.example;
 
+import org.example.repo.AddressBookRepository;
+import org.example.repo.BuddyInfoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AddressBookApplication {
     public static void main(String[] args) {
         SpringApplication.run(AddressBookApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner demo(AddressBookRepository books, BuddyInfoRepository buddies) {
+        return args -> {
+            AddressBook book = books.save(new AddressBook());
+            BuddyInfo nina = new BuddyInfo();
+            nina.setName("Nina");
+            nina.setPhone("+15559990100");
+            BuddyInfo alex = new BuddyInfo();
+            alex.setName("Alex");
+            alex.setPhone("+15558880200");
+            book.addBuddy(nina);
+            book.addBuddy(alex);
+            books.save(book);
+
+            System.out.println("All buddies:");
+            buddies.findAll().forEach(System.out::println);
+
+            System.out.println("Find by name Nina:");
+            buddies.findByName("Nina").forEach(System.out::println);
+        };
     }
 }
--- src/test/java/org/example/AddressBookTest.java
+++ src/test/java/org/example/AddressBookTest.java
@@ -8,7 +8,7 @@ public class AddressBookTest {
     public void addOperates() {
         AddressBook ab = new AddressBook();
         BuddyInfo b = new BuddyInfo();
-        ab.add(b);
+        ab.addBuddy(b);
         assertEquals(1, ab.size());
         assertSame(b, ab.get(0));
     }
@@ -17,9 +17,9 @@ public class AddressBookTest {
     public void removeMiddleObjectOperates() {
         AddressBook ab = new AddressBook();
         BuddyInfo a = new BuddyInfo(), b = new BuddyInfo(), c = new BuddyInfo();
-        ab.add(a);
-        ab.add(b);
-        ab.add(c);
+        ab.addBuddy(a);
+        ab.addBuddy(b);
+        ab.addBuddy(c);
         ab.remove(b);
         assertSame(a, ab.get(0));
         assertSame(c, ab.get(1));
@@ -32,7 +32,7 @@ public class AddressBookTest {
         BuddyInfo b = new BuddyInfo();
         b.setName("Nina");
         b.setPhone("1111111111");
-        ab.add(b);
+        ab.addBuddy(b);
         assertSame(b, ab.get(0));
     }
 
@@ -40,7 +40,7 @@ public class AddressBookTest {
     public void sizeWorks() {
         AddressBook ab = new AddressBook();
         assertEquals(0, ab.size());
-        ab.add(new BuddyInfo());
+        ab.addBuddy(new BuddyInfo());
         assertEquals(1, ab.size());
     }
 
@@ -50,7 +50,7 @@ public class AddressBookTest {
         BuddyInfo b = new BuddyInfo();
         b.setName("Nina");
         b.setPhone("1111111111");
-        ab.add(b);
+        ab.addBuddy(b);
         assertSame(b, ab.getBuddies().get(0));
     }
 }
